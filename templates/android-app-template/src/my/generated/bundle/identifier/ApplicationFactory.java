475617819827d4d8baa91bd6e4577eace6ec79b1
Sven Meyer <s.meyer@itude.com>
2014-08-13 09:35:00 +0200

    added standard templates for iOS and Android

---
+++ templates/android-app-template/src/my/generated/bundle/identifier/ApplicationFactory.java
@@ -0,0 +1,10 @@
+/* 
+__COMMENT_HEADING__
+ */
+package __BUNDLE_IDENTIFIER__;
+
+import com.itude.mobile.mobbl.core.controller.MBApplicationFactory;
+
+public class ApplicationFactory extends MBApplicationFactory {
+	
+}
---
+++ templates/ios-app-template-old/src/xcode/__PROJECT NAME__/Classes/__CLASS_PREFIX__ApplicationFactory.h
@@ -0,0 +1,10 @@
+/*
+ __COMMENT_HEADING__
+ */
+
+
+@interface __CLASS_PREFIX__ApplicationFactory : MBApplicationFactory {
+
+}
+
+@end
