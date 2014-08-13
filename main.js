a655cfe26174fb0a07d2bca94c265dd6a1f25785
Sven Meyer <s.meyer@itude.com>
2014-08-13 23:33:10 +0200

    iOS works, android now also works (including package directory hierarchy)

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 // iOS MOBBL project generator
 
-var helpers = require('./lib/helpers');
-var walkInOrder = require('./lib/dirwalker');
-
-var assert = require('assert');
-var path = require('path');
+/* internal dependencies */
 var program = require('commander');
+var ProjectGenerator = require('./lib/projectgenerator');
 
-(function () {
-  var applyTable = require('./lib/applyTable');
-  helpers.patchPrototype(Object, 'applyTable', applyTable);
-}());
+/* external dependencies */
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
 
 /* command line stuff */
 
@@ -21,7 +19,19 @@ program
 
 /* application specific stuff! yeah! */
 
-var replacements = {
+
+var iosPathTransforms = {
+  '__PROJECT_NAME__': 'itude-mobile-ios-mobbl-custom-view-controller-sample',
+  '__CLASS_PREFIX__': 'CVC'
+};
+
+var androidPathTransforms = {
+  '__PROJECT_NAME__': 'itude-mobile-ios-mobbl-custom-view-controller-sample',
+  '^src/': 'src/com/itude/mobile/mobbl/customviewcontrollersample/',
+};
+
+
+var textTransforms = {
   '__PROJECT_NAME__': 'itude-mobile-ios-mobbl-custom-view-controller-sample',
   '__BUNDLE_DISPLAY_NAME__': 'Custom View Controller Sample App',
   '__BUNDLE_IDENTIFIER__': 'com.itude.mobile.mobbl.customviewcontrollersample',
@@ -39,93 +49,31 @@ var replacements = {
   '__MVN_DEVELOPER_NAME__': 'Sven Meyer'
 };
 
-var fs = require('fs');
-
 var templatePathPerPlatform = {
   ios : './templates/ios-app-template/',
   android : './templates/android-app-template/'
 };
-var stagePathPerPlatform = {
+
+var targetPathPerPlatform = {
   ios : '/tmp/generated-ios-project/',
   android : '/tmp/generated-android-project/'
 }
 
-var replaceStream = require('replacestream');
-var istextorbinary = require('istextorbinary');
+//var iosGenerator = new ProjectGenerator({
+  //templateDirectory: templatePathPerPlatform.ios,
+  //targetDirectory: targetPathPerPlatform.ios,
+  //pathTransforms: iosPathTransforms,
+  //textTransforms: textTransforms
+//});
 
-var templatePath;
-var stagePath; 
-var platforms = ['ios'/*, 'android'*/];
-
-for (var platform in platforms) {
-  templatePath = templatePathPerPlatform[platforms[platform]];
-  stagePath = stagePathPerPlatform[platforms[platform]];
-
-  if (fs.existsSync(stagePath)) {
-    console.log(stagePath + ' already exists');
-    process.exit(1);
-  }
-
-  console.log("STAGEPATH: " + platform);
-
-  fs.mkdirSync(stagePath, '0755');
-
-  walkInOrder(templatePath, function (filePath, attributes) {
-    assert.equal(typeof filePath, 'string');
-    assert.equal(typeof attributes, 'object');
-
-    var isDirectory = attributes.isDir;
-    var isText = !isDirectory && istextorbinary.isTextSync(filePath, fs.readFileSync(filePath));
-    var targetPath = path.join(stagePath, path.relative(templatePath, filePath));
-    var processedPath = targetPath.applyTable(replacements, function (intermediateResult, currentKey, currentValue, replacementsTable) {
-      console.log("INT: " + intermediateResult);
-      regex = new RegExp(currentKey, 'g');
-      return intermediateResult.replace(regex, currentValue);
-    });
-
-    console.log('%s%s filePath: %s', (isDirectory ? 'd' : ' '), (isText ? 't' : (!isDirectory ? 'b' : ' ')), processedPath);
-
-    var readStream = null;
-    var writeStream = null;
-
-    if (isDirectory) {
-      fs.mkdirSync(processedPath, '0755');
-    }
+//iosGenerator.generate();
 
 
-    else if (isText) {
-      console.log("copying from %s to %s", filePath, processedPath);
-      readStream = fs.createReadStream(filePath);
-      writeStream = fs.createWriteStream(processedPath);
+var androidGenerator = new ProjectGenerator({
+  templateDirectory: templatePathPerPlatform.android,
+  targetDirectory: targetPathPerPlatform.android,
+  pathTransforms: androidPathTransforms,
+  textTransforms: textTransforms
+});
 
-      readStream.on('error', function (err) {
-        console.log(err);
-      });
-
-      writeStream.on('error', function (err) {
-        console.log(err);
-      });
-
-      readStream = readStream.applyTable(replacements, function (intermediateResult, currentKey, currentValue) {
-        return intermediateResult.pipe(replaceStream(currentKey, currentValue));
-      });
-
-      readStream.pipe(writeStream);
-    } else {
-      console.log("copying from %s to %s", filePath, processedPath);
-      readStream = fs.createReadStream(filePath);
-      writeStream = fs.createWriteStream(processedPath);
-
-      readStream.on('error', function (err) {
-        console.log(err);
-      });
-
-      writeStream.on('error', function (err) {
-        console.log(err);
-      });
-
-      readStream.pipe(writeStream);
-    }
-
-  });
-}
+androidGenerator.generate();
