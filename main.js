6ef0c170d1d19bacbb10ecf01d42d6bbd502188e
Sven Meyer <s.meyer@itude.com>
2014-08-15 00:34:56 +0200

    changed all examples to something neutral and cleaned up some unused require()'s

--- main.js
+++ main.js
@@ -1,52 +1,43 @@
 // iOS MOBBL project generator
 
 /* internal dependencies */
-var program = require('commander');
 var ProjectGenerator = require('./lib/projectgenerator');
 
 /* external dependencies */
-var fs = require('fs');
-var path = require('path');
-var assert = require('assert');
-
-
-/* command line stuff */
+var program = require('commander');
 
 program
     .version('0.1')
     .option('-p, --project-name <name>', 'The name of the iOS project')
     .parse(process.argv);
 
-/* application specific stuff! yeah! */
-
-
 var iosPathTransforms = {
-  '__PROJECT_NAME__': 'itude-mobile-ios-mobbl-custom-view-controller-sample',
-  '__CLASS_PREFIX__': 'CVC'
+  '__PROJECT_NAME__': 'my-generated-app-project',
+  '__CLASS_PREFIX__': 'GEN'
 };
 
 var androidPathTransforms = {
-  '__PROJECT_NAME__': 'itude-mobile-ios-mobbl-custom-view-controller-sample',
-  '^src/': 'src/com/itude/mobile/mobbl/customviewcontrollersample/',
+  '__PROJECT_NAME__': 'my-generated-app-project',
+  '^src/': 'src/com/example/',
 };
 
 
 var textTransforms = {
-  '__PROJECT_NAME__': 'itude-mobile-ios-mobbl-custom-view-controller-sample',
-  '__BUNDLE_DISPLAY_NAME__': 'Custom View Controller Sample App',
-  '__BUNDLE_IDENTIFIER__': 'com.itude.mobile.mobbl.customviewcontrollersample',
-  '__COMMENT_HEADING__': 'yeah comments!',
-  '__CLASS_PREFIX__': 'CVC',
-  '__MVN_ARTIFACT_ID__': 'itude-mobile-ios-mobbl-custom-view-controller-sample',
-  '__MVN_GROUP_IDENTIFIER__': 'com.itude.mobile.ios.mobbl',
-  '__MVN_PROJECT_DESCRIPTION__': 'This project demonstrates the use case of custom view controllers',
-  '__MVN_PROJECT_URL__': 'http://mobbl.org/',
+  '__PROJECT_NAME__': 'my-generated-app-project',
+  '__BUNDLE_DISPLAY_NAME__': 'My Generated App',
+  '__BUNDLE_IDENTIFIER__': 'com.example',
+  '__COMMENT_HEADING__': 'These comments were automatically generated!',
+  '__CLASS_PREFIX__': 'GEN',
+  '__MVN_ARTIFACT_ID__': 'my-generated-app-project',
+  '__MVN_GROUP_IDENTIFIER__': 'com.example',
+  '__MVN_PROJECT_DESCRIPTION__': 'This project demonstrates the use case of generated app projects',
+  '__MVN_PROJECT_URL__': 'http://example.com/',
   '__MVN_INCEPTION_YEAR__ ': '2014',
-  '__ORGANIZATION_NAME__': 'Itude Mobile B.V.',
-  '__MVN_ORGANIZATION_URL__': 'http://www.itude.com/',
-  '__MVN_PRODUCT_LICENSE__': 'The Apache Software License, Version 2.0',
+  '__ORGANIZATION_NAME__': 'My Generated Company Ltd.',
+  '__MVN_ORGANIZATION_URL__': 'http://www.example.com/',
+  '__MVN_PRODUCT_LICENSE__': 'The Generated Software License',
   '__MVN_LICENSE_URL__': 'LICENSE',
-  '__MVN_DEVELOPER_NAME__': 'Sven Meyer'
+  '__MVN_DEVELOPER_NAME__': 'P. Generator'
 };
 
 var templatePathPerPlatform = {
@@ -59,14 +50,14 @@ var targetPathPerPlatform = {
   android : '/tmp/generated-android-project/'
 }
 
-//var iosGenerator = new ProjectGenerator({
-  //templateDirectory: templatePathPerPlatform.ios,
-  //targetDirectory: targetPathPerPlatform.ios,
-  //pathTransforms: iosPathTransforms,
-  //textTransforms: textTransforms
-//});
+var iosGenerator = new ProjectGenerator({
+  templateDirectory: templatePathPerPlatform.ios,
+  targetDirectory: targetPathPerPlatform.ios,
+  pathTransforms: iosPathTransforms,
+  textTransforms: textTransforms
+});
 
-//iosGenerator.generate();
+iosGenerator.generate();
 
 
 var androidGenerator = new ProjectGenerator({
